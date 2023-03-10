330e6f1aaa47c5ec479774e7e798d6d1cd45f48e
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-03-10 13:37:13 +0900

    fix(enable-minimized-playback): remove integrated disable-shorts-player-pip patch

--- src/main/kotlin/app/revanced/patches/youtube/misc/minimizedplayback/bytecode/patch/MinimizedPlaybackBytecodePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/minimizedplayback/bytecode/patch/MinimizedPlaybackBytecodePatch.kt
@@ -6,7 +6,6 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.data.toMethodWalker
 import app.revanced.patcher.extensions.addInstructions
 import app.revanced.patcher.extensions.instruction
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprintResult
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
@@ -14,30 +13,22 @@ import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.youtube.misc.minimizedplayback.bytecode.fingerprints.*
-import app.revanced.patches.youtube.misc.playertype.patch.PlayerTypeHookPatch
 import app.revanced.patches.youtube.misc.resourceid.patch.SharedResourcdIdPatch
 import app.revanced.shared.annotation.YouTubeCompatibility
 import app.revanced.shared.extensions.toErrorResult
 import app.revanced.shared.util.integrations.Constants.MISC_PATH
 import org.jf.dexlib2.iface.instruction.ReferenceInstruction
-import org.jf.dexlib2.iface.instruction.TwoRegisterInstruction
 import org.jf.dexlib2.iface.reference.MethodReference
 
 @Name("enable-minimized-playback-bytecode-patch")
-@DependsOn(
-    [
-        PlayerTypeHookPatch::class,
-        SharedResourcdIdPatch::class
-    ]
-)
+@DependsOn([SharedResourcdIdPatch::class])
 @YouTubeCompatibility
 @Version("0.0.1")
 class MinimizedPlaybackBytecodePatch : BytecodePatch(
     listOf(
         KidsMinimizedPlaybackPolicyControllerFingerprint,
         MinimizedPlaybackManagerFingerprint,
-        MinimizedPlaybackSettingsFingerprint,
-        PipControllerFingerprint
+        MinimizedPlaybackSettingsFingerprint
     )
 ) {
     override fun execute(context: BytecodeContext): PatchResult {
@@ -53,17 +44,12 @@ class MinimizedPlaybackBytecodePatch : BytecodePatch(
         methods[1].hookPlayback()
         methods[2].walkMutable(context)
 
-        PipControllerFingerprint.result?.hookShortsPiP()?:return PipControllerFingerprint.toErrorResult()
-
         return PatchResultSuccess()
     }
 
     private companion object {
         const val INTEGRATIONS_PLAYBACK_METHOD_REFERENCE =
             "$MISC_PATH/MinimizedPlaybackPatch;->enableMinimizedPlayback()Z"
-
-        const val INTEGRATIONS_PIP_METHOD_REFERENCE =
-            "$MISC_PATH/MinimizedPlaybackPatch;->isNotPlayingShorts(Z)Z"
 
         fun MutableMethod.walkMutable(
             context: BytecodeContext) {
@@ -98,19 +84,6 @@ class MinimizedPlaybackBytecodePatch : BytecodePatch(
                     return-void
                 """, listOf(ExternalLabel("default", instruction(0)))
             )
-        }
-
-        fun MethodFingerprintResult.hookShortsPiP() {
-            val endIndex = scanResult.patternScanResult!!.endIndex
-            with (mutableMethod) {
-                val register = (implementation!!.instructions[endIndex] as TwoRegisterInstruction).registerA
-                addInstructions(
-                    endIndex + 1, """
-                        invoke-static {v$register}, $INTEGRATIONS_PIP_METHOD_REFERENCE
-                        move-result v$register
-                    """
-                )
-            }
         }
     }
 }
