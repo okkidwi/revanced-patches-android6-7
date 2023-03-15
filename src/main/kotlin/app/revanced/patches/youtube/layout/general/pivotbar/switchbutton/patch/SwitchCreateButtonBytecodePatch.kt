eb25d48e4a7113b632ec24202e0412bc105dad69
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-03-15 21:46:12 +0900

    Add `switch-create-notification` patch (from 2.164.15)

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/pivotbar/switchbutton/patch/SwitchCreateButtonBytecodePatch.kt
@@ -0,0 +1,41 @@
+package app.revanced.patches.youtube.layout.general.pivotbar.switchbutton.patch
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patches.youtube.layout.general.pivotbar.switchbutton.fingerprints.AutoMotiveFingerprint
+import app.revanced.shared.annotation.YouTubeCompatibility
+import app.revanced.shared.extensions.toErrorResult
+import app.revanced.shared.util.integrations.Constants.GENERAL_LAYOUT
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+
+@Name("switch-create-notification-bytecode-patch")
+@YouTubeCompatibility
+@Version("0.0.1")
+class SwitchCreateButtonBytecodePatch : BytecodePatch(
+    listOf(AutoMotiveFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        AutoMotiveFingerprint.result?.let {
+            with(it.mutableMethod) {
+                val insertIndex = it.scanResult.patternScanResult!!.endIndex
+                val register = (instruction(insertIndex) as OneRegisterInstruction).registerA
+
+                addInstructions(
+                    insertIndex, """
+                        invoke-static {v$register}, $GENERAL_LAYOUT->switchCreateNotification(Z)Z
+                        move-result v$register
+                        """
+                )
+            }
+        } ?: return AutoMotiveFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+}
---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/pivotbar/switchbutton/patch/SwitchCreateButtonPatch.kt
@@ -0,0 +1,48 @@
+package app.revanced.patches.youtube.layout.general.pivotbar.switchbutton.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.shared.annotation.YouTubeCompatibility
+import app.revanced.shared.util.resources.ResourceHelper
+
+@Patch
+@Name("switch-create-notification")
+@Description("Switching the create button and notification button.")
+@DependsOn(
+    [
+        SwitchCreateButtonBytecodePatch::class,
+        SettingsPatch::class
+    ]
+)
+@YouTubeCompatibility
+@Version("0.0.1")
+class SwitchCreateButtonPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+
+        /*
+         add settings
+         */
+        ResourceHelper.addSettings2(
+            context,
+            "PREFERENCE_CATEGORY: REVANCED_SETTINGS",
+            "PREFERENCE: LAYOUT_SETTINGS",
+            "PREFERENCE_HEADER: GENERAL",
+            "SETTINGS: SWITCH_CREATE_NOTIFICATION"
+        )
+
+        ResourceHelper.patchSuccess(
+            context,
+            "switch-create-notification"
+        )
+
+        return PatchResultSuccess()
+    }
+}
