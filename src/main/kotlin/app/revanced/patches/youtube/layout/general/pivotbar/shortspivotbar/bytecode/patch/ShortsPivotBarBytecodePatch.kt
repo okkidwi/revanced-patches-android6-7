b9c7fb3497fadc39a70967c8650990f94a7d1c69
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-03-30 15:36:34 +0900

    Add `hide-shorts-pivot-bar` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/pivotbar/shortspivotbar/bytecode/patch/ShortsPivotBarBytecodePatch.kt
@@ -0,0 +1,79 @@
+package app.revanced.patches.youtube.layout.general.pivotbar.shortspivotbar.bytecode.patch
+
+import app.revanced.shared.extensions.toErrorResult
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.data.toMethodWalker
+import app.revanced.patcher.extensions.addInstruction
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.shared.fingerprints.PivotBarFingerprint
+import app.revanced.patches.youtube.layout.general.pivotbar.shortspivotbar.bytecode.fingerprints.*
+import app.revanced.shared.annotation.YouTubeCompatibility
+import app.revanced.shared.util.integrations.Constants.GENERAL_LAYOUT
+import org.jf.dexlib2.dexbacked.reference.DexBackedTypeReference
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+
+@Name("hide-shorts-pivot-bar-bytecode-patch")
+@YouTubeCompatibility
+@Version("0.0.1")
+class ShortsPivotBarBytecodePatch : BytecodePatch(
+    listOf(
+        PivotBarFingerprint,
+        ReelWatchBundleFingerprint,
+        ReelWatchEndpointParentFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        PivotBarFingerprint.result?.let { parentResult ->
+            SetPivotBarFingerprint.also { it.resolve(context, parentResult.classDef) }.result?.let {
+                with (it.mutableMethod) {
+                    val startIndex = it.scanResult.patternScanResult!!.startIndex
+                    val instructions = implementation!!.instructions
+
+                    val indexReference = ((instructions[startIndex] as ReferenceInstruction).reference as DexBackedTypeReference).toString()
+                    if (indexReference != targetReference) return SetPivotBarFingerprint.toErrorResult()
+                    val register = (instructions[startIndex] as OneRegisterInstruction).registerA
+
+                    addInstruction(
+                        startIndex + 1,
+                        "sput-object v$register, $GENERAL_LAYOUT->pivotbar:$targetReference"
+                    )
+                }
+            } ?: return SetPivotBarFingerprint.toErrorResult()
+        } ?: return PivotBarFingerprint.toErrorResult()
+
+        ReelWatchBundleFingerprint.result?.let {
+            with (context
+                .toMethodWalker(it.method)
+                .nextMethod(it.scanResult.patternScanResult!!.endIndex, true)
+                .getMethod() as MutableMethod
+            ) {
+                addInstruction(
+                    0,
+                    "invoke-static {}, $GENERAL_LAYOUT->hideShortsPlayerPivotBar()V"
+                )
+            }
+        } ?: return ReelWatchBundleFingerprint.toErrorResult()
+
+        ReelWatchEndpointParentFingerprint.result?.let { parentResult ->
+            ReelWatchEndpointFingerprint.also { it.resolve(context, parentResult.classDef) }.result?.mutableMethod?.addInstruction(
+                0,
+                "sput-object p1, $GENERAL_LAYOUT->shortsContext:Landroid/content/Context;"
+            ) ?: return ReelWatchEndpointFingerprint.toErrorResult()
+        } ?: return ReelWatchEndpointParentFingerprint.toErrorResult()
+
+        return PatchResultSuccess()
+    }
+
+    private companion object {
+        const val targetReference =
+            "Lcom/google/android/apps/youtube/app/ui/pivotbar/PivotBar;"
+    }
+}
---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/pivotbar/shortspivotbar/resource/patch/ShortsPivotBarPatch.kt
@@ -0,0 +1,52 @@
+package app.revanced.patches.youtube.layout.general.pivotbar.shortspivotbar.resource.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.youtube.layout.general.pivotbar.shortspivotbar.bytecode.patch.ShortsPivotBarBytecodePatch
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.shared.annotation.YouTubeCompatibility
+import app.revanced.shared.util.resources.ResourceHelper
+
+@Patch
+@Name("hide-shorts-pivot-bar")
+@Description("Hides the bottom navigation tabs when playing shorts.")
+@DependsOn(
+    [
+        SettingsPatch::class,
+        ShortsPivotBarBytecodePatch::class
+    ]
+)
+@YouTubeCompatibility
+@Version("0.0.1")
+class ShortsPivotBarPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+
+        /*
+         add settings
+         */
+        ResourceHelper.addSettings4(
+            context,
+            "PREFERENCE_CATEGORY: REVANCED_SETTINGS",
+            "PREFERENCE: LAYOUT_SETTINGS",
+            "PREFERENCE_HEADER: GENERAL",
+            "SETTINGS: SHORTS_COMPONENT.PARENT",
+            "SETTINGS: SHORTS_COMPONENT_PARENT.B",
+            "SETTINGS: HIDE_SHORTS_PLAYER_PIVOT_BAR"
+        )
+
+        ResourceHelper.patchSuccess(
+            context,
+            "hide-shorts-pivot-bar"
+        )
+
+        return PatchResultSuccess()
+    }
+
+}
