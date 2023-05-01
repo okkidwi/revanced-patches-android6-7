f652f6eacbb91f1df06e4fcf7849ca9ac00ec7bb
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-05-01 13:25:15 +0900

    Remove force-new-layout patch
    Because the new layout has crash issue

--- src/main/kotlin/app/revanced/patches/youtube/extended/forcenewlayout/fingerprints/ForceNewLayoutFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.youtube.extended.forcenewlayout.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object ForceNewLayoutFingerprint : MethodFingerprint(
-    returnType = "L",
-    parameters = listOf("L"),
-    customFingerprint = { it.definingClass == "Lapp/revanced/integrations/patches/extended/VersionOverridePatch;"}
-)
